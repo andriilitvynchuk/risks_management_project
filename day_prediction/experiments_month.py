a1726a682a1b77c4687b13d57997089febac3a04
Andrii <acethecreator111@gmail.com>
2020-11-09 10:52:06 +0200

    add day prediction rel to prev month

---
+++ day_prediction/experiments_month.py
@@ -0,0 +1,68 @@
+import sys
+from typing import Iterable, NoReturn
+
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import Lasso, LinearRegression, Ridge
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.svm import SVR
+
+
+sys.path.append(".")
+from day_prediction.target import create_days_to_fall_relative_to_previous_month
+from utils.dataset import create_arma_table
+
+
+def print_scores(y_true: Iterable, y_predict: Iterable, prefix: str = "") -> NoReturn:
+    mae = mean_absolute_error(y_true, y_predict)
+    mse = mean_squared_error(y_true, y_predict)
+    r2 = r2_score(y_true, y_predict)
+    print(f"{prefix} MAE: {mae:4f} MSE: {mse:.4f}, R2-score: {r2:.4f}4")
+
+
+def main() -> NoReturn:
+    table = pd.read_csv("./data/tesla_stock.csv", index_col=0)
+    table.index = pd.to_datetime(table.index)
+    variable = table["Close"]
+    percent = 0.02
+
+    x = create_arma_table(variable=variable, p=50, q=50, ma_window=10)
+    x["days_to_fall_relative_to_previous_month"] = create_days_to_fall_relative_to_previous_month(
+        variable=variable, percent=percent
+    )
+    x = x.dropna()
+    x = x.loc["2019":]
+
+    x_train, x_test = x.loc[:"2019-11"], x.loc["2019-12"]
+    x_train, y_train = x_train.iloc[:, :-1], x_train.iloc[:, -1]
+    x_test, y_test = x_test.iloc[:, :-1], x_test.iloc[:, -1]
+
+    models = [
+        LinearRegression(fit_intercept=False, n_jobs=-1),
+        Ridge(fit_intercept=False, alpha=1),
+        Lasso(fit_intercept=False, alpha=1),
+        SVR(),
+        RandomForestRegressor(),
+    ]
+
+    for model in models:
+        model.fit(x_train, y_train)
+        y_predict = model.predict(x_test)
+        print(y_predict, y_test.values)
+
+        class_name = model.__class__.__name__
+        print_scores(y_train, model.predict(x_train), f"{class_name} train month event prediction")
+        print_scores(y_test, y_predict, f"{class_name} test month event prediction")
+        print("")
+
+        plt.plot(y_test.index, y_predict, label=class_name)
+
+    plt.plot(y_test, label="True")
+    plt.legend()
+    plt.title(f"Days to fall by {percent}% relative to previous month")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
--- day_prediction/target.py
+++ day_prediction/target.py
@@ -7,13 +7,36 @@ import pandas as pd
 def create_days_to_fall_relative_to_current_day(variable: pd.Series, percent: float = 0.02) -> pd.Series:
     target: List[int] = []
     for index in range(len(variable) - 1):
-        # target.append(int((variable[index] - variable[index + 1]) / variable[index] > percent))
         fall_happened = False
         for add_index in range(index + 1, len(variable)):
             if (variable[index] - variable[add_index]) > percent:
                 target.append(add_index - index)
                 fall_happened = True
                 break
+        if not fall_happened:
+            target.append(np.nan)
+    target.append(np.nan)
+    return pd.Series(target, index=variable.index)
+
+
+def create_days_to_fall_relative_to_previous_month(variable: pd.Series, percent: float = 0.02) -> pd.Series:
+    month_mean = variable.groupby([variable.index.year, variable.index.month]).mean()
+    target: List[int] = []
+    for index in range(len(variable) - 1):
+        fall_happened = False
+        prev_month_date = variable.index[index] - pd.Timedelta(31, "D")
+        try:
+            prev_month_average = month_mean.loc[(prev_month_date.year, prev_month_date.month)]
+        except KeyError:
+            target.append(np.nan)
+            continue
+
+        for add_index in range(index + 1, len(variable)):
+            if (prev_month_average - variable[add_index]) > percent:
+                target.append(add_index - index)
+                fall_happened = True
+                break
+
         if not fall_happened:
             target.append(np.nan)
     target.append(np.nan)
